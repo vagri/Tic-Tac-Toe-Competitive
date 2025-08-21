8857930bd89df127c143d53eecf6e31645ac21d9
vagri <vagri1999@yahoo.gr>
2025-08-21 12:23:33 +0300

    API almost finished

--- src/main/java/tttc/controller/GameController.java
+++ src/main/java/tttc/controller/GameController.java
@@ -1,11 +1,33 @@
 package tttc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import tttc.model.Player;
+import tttc.service.GameService;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/game")
-@CrossOrigin(origins = "http://localhost:5500") // allow your frontend port
 public class GameController {
 
+    @Autowired
+    private GameService gameService;
+
+    @PostMapping("/start")
+    @ResponseBody
+    public Map<String, Object> startGame() {
+        gameService.startNewGame();
+        Player p1 = gameService.getPlayer1();
+        Player p2 = gameService.getPlayer2();
+
+        return Map.of(
+                "player1Name", p1.getName(),
+                "player1Rank", p1.getRank(),
+                "player2Name", p2.getName(),
+                "player2Rank", p2.getRank()
+        );
     }
 
+
+}
